2ef515e7a15e092144c41a3a0688e3f3d21bd388
Yuri Rykhlo <yurirykhlo@yuris-air.lan>
2021-05-22 18:57:10 -0500

    added Entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown2 import Markdown
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
-    })
+})
+
+def entry(request, title):
+    entries = util.list_entries()
+    if title in entries:
+        page = util.get_entry(title)
+        page_converted = Markdown().convert(page)
+        content = {
+            "title" : title,
+            "page" : page_converted,
+        }
+        return render(request, "encyclopedia/entry.html", content)
+
+    else: #TODO
+        return 
+
 
